05ec5e93102f63f2a4f741f7ef662fd706a38eb0
Michel-Rooney <michelrooney16@gmail.com>
2023-10-11 21:10:46 -0300

    Aula 02

--- src/main/java/br/com/michelrooney/todolist/user/UserModel.java
+++ src/main/java/br/com/michelrooney/todolist/user/UserModel.java
@@ -1,7 +1,59 @@
 package br.com.michelrooney.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
+
+
+/* 
+O Data do lombok irar abstrair todos os geters e setters
+Pode se esficar somente o @Getter ou os @Setter
+
+public String getUsername() {
+    return username;
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+*/
