6262080a0ac05f66e76f01dee8c5d2f0094df196
Michel-Rooney <michelrooney16@gmail.com>
2023-10-11 23:46:04 -0300

    Aula 03 - Implementando segurança nos dados do usuário

---
+++ src/main/java/br/com/michelrooney/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.michelrooney.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository  extends JpaRepository<TaskModel, UUID>{
+    
+}
